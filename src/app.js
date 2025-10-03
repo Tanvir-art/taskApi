a9d9b97f70839cafd023d624ceaceeae898b8a2b
Tanvir Hasan <tanvirrmstu@gmail.com>
2025-10-03 11:31:14 +0600

    live the frontend so change the cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   // origin: 'http://localhost:3000', 
-  origin: 'https://task-api-frontend-git-main-tanvir-haans-projects.vercel.app',
+  origin: 'https://task-api-frontend.vercel.app',
   credentials: true  
 }));
 
